94391c23a624f533fcc105d08cd01edbf02ed3be
Arthurmuzondo <95529080+Arthurmuzondo@users.noreply.github.com>
2023-09-22 12:30:25 +0200

    Update settings.py

--- greatcartprj/settings.py
+++ greatcartprj/settings.py
@@ -147,7 +147,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_HOST_USER = 'artamuzzie@gmail.com'
-EMAIL_HOST_PASSWORD = 'mcsd xojb lkpq pnqg'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
